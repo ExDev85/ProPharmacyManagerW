8ef125a6067b4bcee342e1b142099d639e8c4f2f
ahmed <ahmedflix25@gmail.com>
2016-09-01 13:03:53 +0200

    New Logo and new control the programs gets new icon and WaterMarkCombobox control

---
+++ ProPharmacyManagerW/Controls/WaterMarkComboBox.cs
@@ -0,0 +1,36 @@
+﻿// <copyright>
+//      This work is licensed under the
+//      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//      To view a copy of this license, visit
+//      http://creativecommons.org/licenses/by-nc-sa/4.0/.
+// </copyright>
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ProPharmacyManagerW.Controls
+{
+    public class WaterMarkComboBox : ComboBox
+    {
+        public static readonly DependencyProperty WaterMarkProperty = DependencyProperty.Register("WaterMark", typeof(string), typeof(WaterMarkComboBox), new PropertyMetadata(null));
+        public static readonly DependencyProperty WaterMarkColorProperty = DependencyProperty.Register("WaterMarkColor", typeof(SolidColorBrush), typeof(WaterMarkComboBox), new PropertyMetadata(Brushes.Gray));
+
+        static WaterMarkComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WaterMarkComboBox), new FrameworkPropertyMetadata(typeof(WaterMarkComboBox)));
+        }
+
+        public string WaterMark
+        {
+            get { return (string)GetValue(WaterMarkProperty); }
+            set { SetValue(WaterMarkProperty, value); }
+        }
+
+        public SolidColorBrush WaterMarkColor
+        {
+            get { return (SolidColorBrush)GetValue(WaterMarkColorProperty); }
+            set { SetValue(WaterMarkColorProperty, value); }
+        }
+
+    }
+}
--- ProPharmacyManagerW/ViewModel/NoDecimal.cs
+++ ProPharmacyManagerW/ViewModel/NoDecimal.cs
@@ -5,7 +5,6 @@
 //      http://creativecommons.org/licenses/by-nc-sa/4.0/.
 // </copyright>
 using System;
-using System.Text.RegularExpressions;
 using System.Windows.Data;
 
 namespace ProPharmacyManagerW.ViewModel
